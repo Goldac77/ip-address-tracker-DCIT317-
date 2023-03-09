1b81c03c9921f0dfd560dc3a54b51e8298725aea
Nicholas Amemazior <amemaziornicholas77@gmail.com>
2023-03-09 20:34:18 +0000

    basic functionality done

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 const btn = document.querySelector("#form-btn");
+const ipInput = document.querySelector("#inputBtn");
+const intialLocation = [5.652426305999822, -0.1880851446149894]; //UGCS location
+
+
 btn.addEventListener("click", () => {
-    map.flyTo([5.5345, -0.4168], 13);
+    fetch(`http://ipinfo.io/${ipInput.value}?token=54737db2987f24`)
+    .then(response => response.json())
+    .then(data => {
+        //get the latitude and longitude...
+        let location = data.loc.split(",");
+        let lat = location[0];
+        let long = location[1];
+
+        //fly to new location
+        map.flyTo([lat, long], 13);
+
+        //remove initial marker
+        map.removeLayer(initialMarker);
+
+        //new marker
+        let newMarker = new L.Marker([lat, long]);
+        newMarker.addTo(map);
+        newMarker.bindPopup(`${data.org}`);
+
+    })
 })
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+//Initial map render location is UGCS
+var map = L.map('map').setView(intialLocation, 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map)
+//marker marker to map
+var initialMarker = new L.Marker(intialLocation);
+initialMarker.addTo(map);
+//add marker popup
+initialMarker.bindPopup("University of Ghana Computing Systems (UGCS)")
